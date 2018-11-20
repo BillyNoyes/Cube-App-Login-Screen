7bd0fa4a2d4331322251269df67c9c64c93283ce
William (Billy) Noyes <wbillypn@gmail.com>
2018-11-20 01:19:34 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// https://dribbble.com/shots/3458675-Streaming-Singles-app-unused-element-part-19
+
+$("#login").click(function(){
+        $("input").val("");
+ });
+
+$("#create").click(function(){
+        $("input").val("");
+ });
